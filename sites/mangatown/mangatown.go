fe2ec7d42618e97968c14e817bed566fa2cc14df
Naing Lin Aung <nainglinaung91@gmail.com>
2019-10-18 15:11:18 +0630

    use goroutine

--- sites/mangatown/mangatown.go
+++ sites/mangatown/mangatown.go
@@ -3,6 +3,8 @@ package mangatown
 import (
 	"fmt"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/nainglinaung/gomanga/lib/ehelper"
 )
@@ -14,6 +16,8 @@ var (
 	imageCounter int
 	selector     ehelper.Selector
 	helper       ehelper.Ehelper
+	crawlWg      sync.WaitGroup
+	downloadWg   sync.WaitGroup
 )
 
 func init() {
@@ -29,24 +33,43 @@ func Execute(manga string, chapter int, output string) {
 	folderPath = helper.CreateFolderPath(manga, chapter, output)
 	helper.CreateFolder(folderPath)
 	url := fmt.Sprintf("%smanga/%s/c%03d", url, manga, chapter)
-	crawl(url)
+	crawlWg.Add(1)
+	start := time.Now()
+	downloadChan := make(chan [2]string)
+	go func() {
+		for data := range downloadChan {
+			downloadWg.Add(1)
+			go download(data[0], data[1])
+		}
+	}()
+	go crawl(url, downloadChan)
+	crawlWg.Wait()
+	fmt.Printf("Elasped Time: %#v\n", time.Now().Sub(start).Seconds())
+	downloadWg.Wait()
 }
 
-func crawl(url string) {
+func download(imageLink, fullImagePath string) {
+	defer downloadWg.Done()
+	helper.Download(imageLink, fullImagePath)
+}
+func crawl(url string, downloadChan chan<- [2]string) {
+	defer crawlWg.Done()
 	fileLink := fmt.Sprintf("%s/%d.html", url, imageCounter)
 	println(fileLink)
 	resp := helper.FetchURL(fileLink)
-
 	if resp != nil {
 		imageLink, _ := helper.ParseResponse(resp.Body, selector)
 		if len(imageLink) > 0 {
 			fullImagePath := fmt.Sprintf("%s/%d.jpg", folderPath, imageCounter)
-			fmt.Println(fullImagePath)
-			helper.Download(imageLink, fullImagePath)
+			downloadChan <- [2]string{imageLink, fullImagePath}
 			helper.Log(fullImagePath)
 			imageCounter++
-			crawl(url)
+			crawlWg.Add(1)
+			go crawl(url, downloadChan)
+		} else {
+			close(downloadChan)
 		}
 
 	}
+
 }
